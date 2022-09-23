6d2537a2331198e402f30087bf1a1a3c1079187d
bebo <bebo@pop-os.localdomain>
2022-09-23 14:20:25 +0200

    SPE in atoi finally

--- src/ft_nb/ft_atoi.c
+++ src/ft_nb/ft_atoi.c
@@ -21,7 +21,7 @@ int		ft_atoi(const char *str)
 	i = 0;
 	nbr = 0;
 	neg = 1;
-	while (SPE(str[i]))
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
 	if (str[i] == '+' || str[i] == '-')
 	{
